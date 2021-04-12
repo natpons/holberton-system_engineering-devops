d6f7409598785f1d6e3d62ae9b2fb79f840d05b0
Natalia Ponsard <2503@holbertonschool.com>
2021-04-12 17:10:21 +0000

    task 2 version noir

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+# To automate the task of creating a custom HTTP header response with Puppet
+# https://pptr.dev/#?product=Puppeteer&version=v5.2.1&show=api-pagesetextrahttpheadersheaders
+
+page.setExtraHTTPHeaders([
+['X-Served-By', '$HOSTNAME'],
+]);
