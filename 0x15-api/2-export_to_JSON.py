b7064aebfd9a8d0b2d69ef6aa68a463430dac757
Natalia Ponsard <2503@holbertonschool.com>
2021-05-03 13:55:34 +0000

    changed user_id to userId, corrected comment

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,15 +13,15 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1]
+    userId = argv[1]
 
     # Body.json():Takes a Response stream and reads it to completion
     # Returns: this object could be anything that can be
     # represented by JSON - an object, an array, a string, a number
     user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                        format(user_id)).json()
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos?user_id={}'
-                        .format(user_id)).json()
+                        format(userId)).json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(userId)).json()
     tasks = []
 
     # Serialization:
@@ -33,10 +33,10 @@ if __name__ == "__main__":
         task_dict["username"] = user.get('username')
         tasks.append(task_dict)
 
-    # 2. to write obj python(dict tasks) to the file JSON:
+    # 2. to write obj python(dict json_object) to the file JSON:
     json_object = {}
     # key: value
-    json_object[user_id] = tasks
-    with open('{}.json'.format(user_id), 'w') as json_file:
+    json_object[userId] = tasks
+    with open('{}.json'.format(userId), 'w') as json_file:
         # dict to be serialized and the file to which the bytes will be written
         json.dump(json_object, json_file)
